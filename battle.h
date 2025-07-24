39f5c8a628b893a6ce7525da1cb30495d5af3435
takahashijake <jtakahashi@ucmerced.edu>
2025-07-24 15:13:20 -0700

    Moved battle sequence into seperate header/cpp file

---
+++ battle.h
@@ -0,0 +1,27 @@
+#ifndef BATTLE_H
+#define BATTLE_H
+
+class Character;
+class Player;
+class RandomGenerator;
+
+#include <memory>
+class BattleSequence{
+    private:
+        Character* thisCharacter;
+        Player* thisPlayer;
+        enum turnState{
+            playerTurn,
+            enemyTurn,
+            gameOver
+        };
+        RandomGenerator* randomNumber;
+
+    public:
+        BattleSequence(const std::unique_ptr<Player>& playerArgument, const std::unique_ptr<Character>& characterArgument);
+        void playerMove(turnState& playerTurn);
+        void enemyMove(turnState& playerTurn);
+        void mainBattle();
+        ~BattleSequence();
+};
+#endif 
